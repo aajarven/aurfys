40fa6cb390040818fe59ff648e9b9443a038e196
aajarven <anni.jarvenpaa@gmail.com>
2016-05-15 17:29:47 +0300

    apumetodeita

--- Delaunay/src/delaunay/QuickhullKolmio.java
+++ Delaunay/src/delaunay/QuickhullKolmio.java
@@ -11,39 +11,40 @@ import java.util.ArrayList;
  *
  * @author anni
  */
-public class QuickhullKolmio extends Kolmio{
-    
+public class QuickhullKolmio extends Kolmio {
+
     private ArrayList<Piste> nakyvatPisteet;
     private ArrayList<QuickhullKolmio> naapurit;
-    
+
     public QuickhullKolmio(Piste p1, Piste p2, Piste p3) {
         super(p1, p2, p3);
         this.nakyvatPisteet = new ArrayList<>();
         this.naapurit = new ArrayList<>();
     }
-    
-    public void lisaaNakyvaPiste(Piste p){
+
+    public void lisaaNakyvaPiste(Piste p) {
         nakyvatPisteet.add(p);
     }
-    
-    public void lisaaNaapuri(QuickhullKolmio n){
+
+    public void lisaaNaapuri(QuickhullKolmio n) {
         naapurit.add(n);
     }
-    
+
     /**
      * Etsii näkyvistä pisteistä kauimpana tasosta sijaitsevan
+     *
      * @return kaukaisin piste
      */
-    public Piste etsiKaukaisin() throws Exception{
-        if (nakyvatPisteet.isEmpty()){
+    public Piste etsiKaukaisin() throws Exception {
+        if (nakyvatPisteet.isEmpty()) {
             throw new Exception("QuickhullKolmiolla ei ole näkyviä pisteitä");
         }
-        
+
         Piste kaukaisin = nakyvatPisteet.get(0);
         double kaukaisimmanEtaisyys = 0;
-        for (Piste p: nakyvatPisteet){
+        for (Piste p : nakyvatPisteet) {
             double kasiteltavanEtaisyys = etaisyysTasosta(p);
-            if (kasiteltavanEtaisyys < kaukaisimmanEtaisyys){
+            if (kasiteltavanEtaisyys < kaukaisimmanEtaisyys) {
                 kaukaisin = p;
                 kaukaisimmanEtaisyys = kasiteltavanEtaisyys;
             }
@@ -54,68 +55,72 @@ public class QuickhullKolmio extends Kolmio{
     public ArrayList<Piste> getNakyvatPisteet() {
         return nakyvatPisteet;
     }
-    
-    public Kolmio toKolmio(){
+
+    public Kolmio toKolmio() {
         return new Kolmio(super.getP1(), super.getP2(), super.getP3());
     }
-    
+
     /**
      * Kertoo, onko piste samalla puolella kolmion virittämää tasoa kuin origo
+     *
      * @param p tutkittava piste
      * @return true jos piste on kauempana origosta kuin taso, muuten false
      */
-    public boolean onKauempanaOrigosta(Piste p){
+    public boolean onKauempanaOrigosta(Piste p) {
         double[] kertoimet = tasonYhtalo();
         double a = kertoimet[0];
         double b = kertoimet[1];
         double c = kertoimet[2];
         double d = kertoimet[3];
-        
-        return (p.x()*a+p.y()*b+p.z()*c-d)*(-d)>0 ; // tarkasta tämä
+
+        return (p.x() * a + p.y() * b + p.z() * c - d) * (-d) > 0; // tarkasta tämä
     }
-    
+
     /**
      * Palauttaa kertoimet tason yhtälöön muodossa ax+by+cz=d
+     *
      * @return [a, b, c, d]
      */
-    private double[] tasonYhtalo(){
+    private double[] tasonYhtalo() {
         Vektori3D v1 = super.getP1().vektoriPisteeseen(super.getP2());
         Vektori3D v2 = super.getP1().vektoriPisteeseen(super.getP3());
         Vektori3D normaali = v1.ristitulo(v2);
-        
+
         double a = normaali.getI();
         double b = normaali.getJ();
         double c = normaali.getK();
-        double d = normaali.getI()*v1.getI() + normaali.getJ()*v1.getJ()+normaali.getK()*v1.getK();
-        
+        double d = normaali.getI() * v1.getI() + normaali.getJ() * v1.getJ() + normaali.getK() * v1.getK();
+
         return new double[]{a, b, c, d};
     }
-    
-        /**
+
+    /**
      * Kertoo pisteen p etäisyyden kolmion virittämästä tasosta
+     *
      * @param p tutkittava piste
      * @return pisteen etäisyys tasosta
      */
-    public double etaisyysTasosta(Piste p){
+    public double etaisyysTasosta(Piste p) {
         double[] kertoimet = tasonYhtalo();
         double a = kertoimet[0];
         double b = kertoimet[1];
         double c = kertoimet[2];
         double d = kertoimet[3];
-        
-        return Math.abs(a*p.x()+b*p.y()+c*p.z()+d)/Math.sqrt(a*a+b*b+c*c);
+
+        return Math.abs(a * p.x() + b * p.y() + c * p.z() + d) / Math.sqrt(a * a + b * b + c * c);
     }
-    
+
     /**
      * Palauttaa annetun kolmion pisteet, jotka eivät ole osa tätä kolmiota
+     *
      * @param k kolmio
      * @return ArrayList jossa 0-3 pistettä
      */
-    public ArrayList<Piste> erillisetPisteet(QuickhullKolmio k){
+    public ArrayList<Piste> erillisetPisteet(QuickhullKolmio k) {
         ArrayList<Piste> yhteiset = new ArrayList<>();
         ArrayList<Piste> omat = super.getPisteet();
-        for(Piste p: k.getPisteet()){
-            if (! omat.contains(p)){
+        for (Piste p : k.getPisteet()) {
+            if (!omat.contains(p)) {
                 yhteiset.add(p);
             }
         }
@@ -125,13 +130,32 @@ public class QuickhullKolmio extends Kolmio{
     public ArrayList<QuickhullKolmio> getNaapurit() {
         return naapurit;
     }
-    
-    public ArrayList<Sivu> getSivut(){
+
+    public ArrayList<Sivu> getSivut() {
         ArrayList<Sivu> palautus = new ArrayList();
         palautus.add(new Sivu(super.getP1(), super.getP2()));
         palautus.add(new Sivu(super.getP1(), super.getP3()));
         palautus.add(new Sivu(super.getP2(), super.getP3()));
         return palautus;
     }
+
+    public boolean sisaltaaPisteen(Piste p) {
+        return super.getPisteet().contains(p);
+    }
     
+    /**
+     * Määrittää tämän ja tutkittavan kolmion yhteisen sivun
+     * @param k toinen kolmio
+     * @return kolmioiden yhteinen sivu tai null jos sellaista ei ole
+     */
+    public Sivu yhteinenSivu(QuickhullKolmio k){
+        ArrayList<Piste> yhteisetPisteet = this.getPisteet();
+        yhteisetPisteet.removeAll(this.erillisetPisteet(k));
+        if (yhteisetPisteet.size() != 2) {
+            return null;
+        } else {
+            return new Sivu(yhteisetPisteet.get(0), yhteisetPisteet.get(1));
+        }
+    }
+
 }
