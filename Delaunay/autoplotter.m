5c3f0c65b6fd82f326d6f3ee303517b00e2b66f3
aajarven <anni.jarvenpaa@gmail.com>
2016-05-20 03:24:33 +0300

    tiedostojen tallennus, pisteet näkyviin

--- Delaunay/autoplotter.m
+++ Delaunay/autoplotter.m
@@ -3,71 +3,93 @@ vaalharm = [0.8, 0.8, 0.8];
 tumpun = [0.5, 0, 0];
 vaalpun = [0.8, 0, 0];
 iteraatio = 0;
+edellinenToteutunut = 1;
+kuva = 0;
 
 plottaaKolmiot('debug/ensimmaiset', tumharm);
-disp('ensimmäinen tetraedri')
-pause;
+hold on;
+plottaaPisteet('debug/pisteet.txt', vaalharm);
+%disp('ensimmäinen tetraedri')
+axis square;
 
 
 while true
-    hold on;
     iteraatio=iteraatio+1;
-    
+    disp(num2str(iteraatio));
     plottaaKolmiot(strcat('debug/tyostettava-',num2str(iteraatio)),tumpun);
-    disp(strcat('iteraation ',num2str(iteraatio),' työstettävä'))
-    pause;
+    print(strcat('data/kolmiointianimaatio/',num2str(kuva)), '-dpng');
+    kuva = kuva+1;
+    %disp(strcat('iteraation ',num2str(iteraatio),' työstettävä'))
+    %pause;
     
     seuraava = strcat('debug/nakyvat-',num2str(iteraatio),'.txt');
     if exist(seuraava, 'file') == 2
-        plottaaPisteet(seuraava,vaalharm);
-        disp(strcat('iteraation ',num2str(iteraatio),' näkyvät pisteet'));
+        plottaaPisteet(seuraava,'black');
+        %disp(strcat('iteraation ',num2str(iteraatio),' näkyvät pisteet'));
+        print(strcat('data/kolmiointianimaatio/',num2str(kuva)), '-dpng');
+        kuva = kuva+1;
     else
-        disp(strcat('iteraatiossa ',num2str(iteraatio),' ei näkyviä pisteitä'));
+        %disp(strcat('iteraatiossa ',num2str(iteraatio),' ei näkyviä pisteitä'));
     end
     
     seuraava = strcat('debug/kaukaisin-',num2str(iteraatio),'.txt');
     if exist(seuraava, 'file') == 2
         plottaaPisteet(seuraava,'red');
-        disp(strcat('iteraation ',num2str(iteraatio),' kaukaisin piste'));
+        %disp(strcat('iteraation ',num2str(iteraatio),' kaukaisin piste'));
+        edellinenToteutunut = iteraatio;
+        print(strcat('data/kolmiointianimaatio/',num2str(kuva)), '-dpng');
+        kuva = kuva+1;
     else
-        disp(strcat('iteraatiossa ',num2str(iteraatio),' ei kaukaisinta'));
+        %disp(strcat('iteraatiossa ',num2str(iteraatio),' ei kaukaisinta'));
     end
-    pause;
+    %pause;
     
     seuraava = strcat('debug/valoisat-',num2str(iteraatio));
     if exist(strcat(seuraava,'-x.txt'), 'file') == 2
         plottaaKolmiot(seuraava,vaalpun);
-        disp(strcat('iteraation ',num2str(iteraatio),' valoisat'))
+        %disp(strcat('iteraation ',num2str(iteraatio),' valoisat'))
+        print(strcat('data/kolmiointianimaatio/',num2str(kuva)), '-dpng');
+        kuva = kuva+1;
     else
-        disp(strcat('iteraatiossa ',num2str(iteraatio),' ei valoisia'));
+        %disp(strcat('iteraatiossa ',num2str(iteraatio),' ei valoisia'));
     end
-    pause;
+    %pause;
     
     seuraava = strcat('debug/horisontti-',num2str(iteraatio));
     if exist(strcat(seuraava,'-x.txt'), 'file') == 2
         plottaaViivat(seuraava, 'cyan');
-        disp(strcat('iteraation ',num2str(iteraatio),' horisontti'))
+        %disp(strcat('iteraation ',num2str(iteraatio),' horisontti'))
+        print(strcat('data/kolmiointianimaatio/',num2str(kuva)), '-dpng');
+        kuva = kuva+1;
     else
-        disp(strcat('iteraatiossa ',num2str(iteraatio),' ei horisonttia'));
+        %disp(strcat('iteraatiossa ',num2str(iteraatio),' ei horisonttia'));
     end
-    pause;
+    %pause;
     
     seuraava = strcat('debug/uudet-',num2str(iteraatio));
     if exist(strcat(seuraava,'-x.txt'), 'file') == 2
         plottaaKolmiot(strcat('debug/uudet-',num2str(iteraatio)),vaalharm);
-        disp(strcat('iteraation ',num2str(iteraatio),' uudet'))
+        %disp(strcat('iteraation ',num2str(iteraatio),' uudet'))
+        print(strcat('data/kolmiointianimaatio/',num2str(kuva)), '-dpng');
+        kuva = kuva+1;
     else
-        disp(strcat('iteraatiossa ',num2str(iteraatio),' ei uusia'));
+        %disp(strcat('iteraatiossa ',num2str(iteraatio),' ei uusia'));
     end
-    pause;
+    %pause;
     
+    hold off;
     seuraava = strcat('debug/palautettavat-',num2str(iteraatio));
     if exist(strcat(seuraava,'-x.txt'), 'file') == 2
-        hold off;
         plottaaKolmiot(seuraava,tumharm);
-        disp(strcat('tilanne iteraation ',num2str(iteraatio),' lopuksi'))
+        %disp(strcat('tilanne iteraation ',num2str(iteraatio),' lopuksi'))
     else
-        disp(strcat('tilanne ei muuttunut iteraatiossa ',num2str(iteraatio)));
+        %disp(strcat('tilanne ei muuttunut iteraatiossa ',num2str(iteraatio)));
+        plottaaKolmiot(strcat('debug/palautettavat-',num2str(edellinenToteutunut)),tumharm);
     end
-    pause;
+    hold on;
+    axis square;
+    plottaaPisteet('debug/pisteet.txt', vaalharm);
+    print(strcat('data/kolmiointianimaatio/',num2str(kuva)), '-dpng');
+    kuva = kuva+1;
+    %pause;
 end
